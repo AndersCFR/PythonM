5c4cf68c0dc52547093d2782be076ec866a84b77
-l <anderson.cardenas.r@gmail.com>
2019-12-26 22:52:55 -0500

    commit all

--- Python_basics/1.DataTyes.py
+++ Python_basics/1.DataTyes.py
@@ -13,4 +13,12 @@ print (6 % 4)# Módulo de división
 
 #Math Functions
 print(round(45.5555,3)) #Redondeando
-
+print(abs(-860)) #Valor Absoluto
+#Operator Precedence
+print(454/8*2+54-87*5)  
+# Complex Numbers
+print("Ejemplo de número complejo: ", complex(5,2))
+# Binary Numbers
+print(bin(255))
+#Binary to Integer
+print(int('0b1011',2))
---
+++ Python_basics/2.Variables.py
@@ -0,0 +1,17 @@
+# Variables
+Saludo = 'Hellooo'
+print(Saludo)
+name1, name2, name3 = 'Anderson', 'Alex', 'Amy' #Multiple declaration
+print(name1, name2, name3)
+
+factorconversion=2.2
+kg=4/factorconversion
+print(kg)
+
+# Augment Assigment Operator
+value = 5
+value += 2 #value=value+2
+value **=2
+print(value)
+
+
