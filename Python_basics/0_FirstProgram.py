d45de94118186f6f4f61d5b7a6521b9239a71bbd
-l <anderson.cardenas.r@gmail.com>
2019-12-26 20:26:08 -0500

    commit all

---
+++ Python_basics/0_FirstProgram.py
@@ -0,0 +1,11 @@
+print('Hello world en Python')
+name = input('What is your name?\n')
+print('Helooo ' + name)
+
+# Fundamental Data Types
+"""int float bool str list tuple set dict"""
+
+# Classes -> custom types
+
+# Specialized Data Types
+#none
---
+++ Python_basics/1.DataTyes.py
@@ -0,0 +1,16 @@
+# Fundamental Data Types
+# INT AND FLOAT
+print(2+4)
+print(2-7)
+print(2*8)
+print(8/4)
+
+print(type(2/9)) # Outputing data types 
+print(type(0.25))
+print (2 ** 2) # Potencia 
+print (2 // 8)  #Divisón aproximada
+print (6 % 4)# Módulo de división 
+
+#Math Functions
+print(round(45.5555,3)) #Redondeando
+
